180de892f864b979895bde0e93b7d84b78665840
aronknox0608 <aronknox0608@gmail.com>
2022-07-21 15:02:22 +0430

    add navigator to restaurant screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app_ui/screens/restaurant_screen.dart';
 
 import '/data/data.dart';
 import '/models/restaurant.dart';
@@ -8,71 +9,78 @@ import '/widgets/recent_orders.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  Widget _buildNearbyRestaurants() {
+  Widget _buildNearbyRestaurants(BuildContext context) {
     List<Widget> restaurantList = [];
     for (Restaurant thisRestaurant in restaurants) {
       restaurantList.add(
-        Container(
-          margin: const EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 10.0,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15.0),
-            border: Border.all(width: 1.0, color: Colors.grey[200]!),
-          ),
-          child: Row(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: Image.asset(
-                  thisRestaurant.imageUrl!,
-                  height: 150.0,
-                  width: 150.0,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(12.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        thisRestaurant.name!,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 4.0),
-                      RatingStars(thisRestaurant.rating!),
-                      const SizedBox(height: 4.0),
-                      Text(
-                        thisRestaurant.address!,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 4.0),
-                      const Text(
-                        '0.2 miles away',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => RestaurantScreen(restaurant: thisRestaurant)),
+            );
+          },
+          child: Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(width: 1.0, color: Colors.grey[200]!),
+            ),
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image.asset(
+                    thisRestaurant.imageUrl!,
+                    height: 150.0,
+                    width: 150.0,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          thisRestaurant.name!,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        RatingStars(thisRestaurant.rating!),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          thisRestaurant.address!,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        const Text(
+                          '0.2 miles away',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
@@ -155,7 +163,7 @@ class HomeScreen extends StatelessWidget {
               ),
 
               // TODO: list of nearby restaurants
-              _buildNearbyRestaurants(),
+              _buildNearbyRestaurants(context),
             ],
           ),
         ],
---
+++ lib/screens/restaurant_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '/models/restaurant.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  final Restaurant? restaurant;
+  const RestaurantScreen({this.restaurant, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
